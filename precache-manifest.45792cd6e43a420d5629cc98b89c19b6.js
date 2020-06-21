779a539d8a082138e77e70de8dcea33e8e6a9059
Saunak <ssaunak256@gmail.com>
2020-06-22 00:26:09 +0530

    Updates

---
+++ precache-manifest.45792cd6e43a420d5629cc98b89c19b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "347d8a7c80b208e81a330d8ca9cd9ae4",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "16610b041736db2934a0",
+    "url": "/covid-tracker/static/css/main.a9ff5390.chunk.css"
+  },
+  {
+    "revision": "004e69ee80142df3ec9f",
+    "url": "/covid-tracker/static/js/2.72d70ef2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.72d70ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16610b041736db2934a0",
+    "url": "/covid-tracker/static/js/main.1d2c174a.chunk.js"
+  },
+  {
+    "revision": "4ab6cc6a6673a41c1175",
+    "url": "/covid-tracker/static/js/runtime-main.51b14c82.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/image.d7265326.png"
+  }
+]);
